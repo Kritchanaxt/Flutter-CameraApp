63a95b556dd9e3493eab6e80786d06e1741b7556
Kritchanaxt <kritchanatweve@gmail.com>
2025-02-19 23:40:34 +0700

    update

---
+++ flutter_cameraApp/lib/cropped_image.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+
+class CroppedImage extends StatefulWidget {
+  final String imagePath; // ใช้ imagePath แทน
+  const CroppedImage({super.key, required this.imagePath});
+
+  @override
+  State<CroppedImage> createState() => _CroppedImageState();
+}
+
+class _CroppedImageState extends State<CroppedImage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Camera App'),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey,
+        foregroundColor: Colors.white,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(5),
+          child: InteractiveViewer(
+            child: Image.file( // ใช้ Image.file แทน Image
+              File(widget.imagePath), // ใช้ path จากการ crop
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
